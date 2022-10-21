6a2fac6412ea752f5afa218de59dff4a0fd6128e
rishabhpatni <rishabh28patni@gmail.com>
2022-10-21 13:51:27 -0700

    added more infrastructure to access twitter API

---
+++ main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 21 12:56:55 2022
+
+@author: rishabhpatni
+"""
+
+from twitter_extract import search_twitter
+import json
+
+query = "climate change"
+max_results = 10
+
+query_params = {'query': query,'tweet.fields': 'text, author_id, created_at', 'max_results': max_results}
+
+
+
+def main():
+    json_response = search_twitter(query_params)
+    print(json.dumps(json_response, indent=4, sort_keys=True))
+
+    
+main()
--- twitter_extract.py
+++ twitter_extract.py
@@ -6,3 +6,32 @@ Created on Wed Oct 19 15:47:03 2022
 @author: rishabhpatni
 """
 
+import requests
+import os
+
+
+
+
+BEARER_TOKEN = os.environ.get("<insert token>")
+
+def bearer_oauth(r):
+    """
+    Method required by bearer token authentication.
+    """
+
+    r.headers["Authorization"] = f"Bearer {BEARER_TOKEN}"
+    r.headers["User-Agent"] = "v2TweetLookupPython"
+    return r
+
+
+
+def search_twitter(query_params):
+
+    url = "https://api.twitter.com/2/tweets/search/recent"
+
+    response = requests.request("GET", url, auth=bearer_oauth, params = query_params)
+    print(response.status_code)
+
+    if response.status_code != 200:
+        raise Exception(response.status_code, response.text)
+    return response.json()
